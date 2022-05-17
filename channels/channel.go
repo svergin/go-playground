e947bd70badfade39ff10061fa83bb6edd587619
Sven Vergin <sven.vergin@nortal.com>
2022-05-17 11:01:34 +0200

    clean up code

--- channels/channel.go
+++ channels/channel.go
@@ -9,26 +9,6 @@ import (
 func Walk(t *tree.Tree, ch chan int) {
 	walk(t, ch)
 	close(ch)
-	// var nodeQueue []*tree.Tree = make([]*tree.Tree, 0)
-	// var aktNode = t
-
-	// for {
-	// 	if aktNode.Left != nil {
-	// 		nodeQueue = append(nodeQueue, aktNode.Left)
-	// 	}
-	// 	if aktNode.Right != nil {
-	// 		nodeQueue = append(nodeQueue, aktNode.Right)
-	// 	}
-	// 	ch <- aktNode.Value
-
-	// 	if len(nodeQueue) > 0 {
-	// 		aktNode = nodeQueue[len(nodeQueue)-1]
-	// 		nodeQueue = nodeQueue[0 : len(nodeQueue)-1]
-	// 	} else {
-	// 		close(ch)
-	// 		break
-	// 	}
-	// }
 }
 
 func walk(t *tree.Tree, ch chan int) {
@@ -70,27 +50,4 @@ func Same(t1, t2 *tree.Tree) bool {
 	}
 	return true
 
-	// t2Values := make([]int, 11)
-
-	// counter := 0
-	// for i := range c2 {
-	// 	t2Values[counter] = i
-	// 	counter++
-	// }
-	// fmt.Println("t2Values: ", t2Values)
-	// for i := range c1 {
-	// 	fmt.Println("i aus c1: ", i)
-	// 	var found bool = false
-	// 	for _, j := range t2Values {
-
-	// 		if i == j {
-	// 			fmt.Printf("Found: %v-%v", i, j)
-	// 			found = true
-	// 			break
-	// 		}
-	// 	}
-	// 	result = found
-	// }
-
-	// return result
 }
