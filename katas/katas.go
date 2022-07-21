8f8c3413b781a9ca7ca1aa1a5d1aeaf6d54ad6f1
Sven Vergin <sven.vergin@nortal.com>
2022-07-21 17:04:00 +0200

    Weitere Katas implementiert

--- katas/katas.go
+++ katas/katas.go
@@ -1,5 +1,10 @@
 package katas
 
+import (
+	"math"
+	"strings"
+)
+
 //Kata: Beginner - Lost Without a Map
 func Maps(x []int) []int {
 	output := make([]int, len(x))
@@ -18,4 +23,38 @@ func SquareSum(numbers []int) int {
 		result += v * v
 	}
 	return result
+}
+
+//Kata: Remove String Spaces
+func NoSpace(word string) string {
+	return strings.ReplaceAll(word, " ", "")
+}
+
+//Kata: Sum of positive
+func PositiveSum(numbers []int) int {
+	var result int = 0
+	for _, v := range numbers {
+		if v >= 0 {
+			result += v
+		}
+	}
+	return result
+}
+
+//Kata: Heron's formula
+func Heron(a, b, c float64) (area float64) {
+	s := (a + b + c) / 2
+	area = math.Sqrt(s * (s - a) * (s - b) * (s - c))
+	return area
+}
+
+//Kata: Get the Middle Character
+func GetMiddle(s string) string {
+	laenge := len(s)
+
+	if laenge%2 == 1 {
+		//TODO
+	}
+
+	return ""
 }
--- katas/katas_test.go
+++ katas/katas_test.go
@@ -8,29 +8,53 @@ import (
 
 func Test_Maps_Should_double_up_int_vals_in_array(t *testing.T) {
 	input := []int{1, 2, 3, 4}
-	output := Maps(input)
+	actual := Maps(input)
 
-	assert.Equal(t, output[0], 2)
-	assert.Equal(t, output[1], 4)
-	assert.Equal(t, output[2], 6)
-	assert.Equal(t, output[3], 8)
+	assert.Equal(t, actual[0], 2)
+	assert.Equal(t, actual[1], 4)
+	assert.Equal(t, actual[2], 6)
+	assert.Equal(t, actual[3], 8)
 
 }
 
 func Test_Square_and_Sum_should_return_correct_result(t *testing.T) {
 	input := []int{1, 2}
-	output := SquareSum(input)
+	actual := SquareSum(input)
 
-	assert.Equal(t, 5, output)
+	assert.Equal(t, 5, actual)
 
 	input = []int{0, 3, 4, 5}
-	output = SquareSum(input)
+	actual = SquareSum(input)
 
-	assert.Equal(t, 50, output)
+	assert.Equal(t, 50, actual)
 
 	input = []int{}
-	output = SquareSum(input)
+	actual = SquareSum(input)
 
-	assert.Equal(t, 0, output)
+	assert.Equal(t, 0, actual)
 
+}
+
+func Test_Spaces_removed_from_string_successfully(t *testing.T) {
+	actual := NoSpace("Das ist ein Test.")
+
+	assert.Equal(t, "DasisteinTest.", actual)
+
+}
+
+func Test_Positive_Numbers_were_correctly_summed_up(t *testing.T) {
+	var toSum []int = []int{1, -2, 3, 4, 5}
+	actual := PositiveSum(toSum)
+
+	assert.Equal(t, 13, actual)
+}
+
+func Test_Herons_formula_calculated_the_correct_result(t *testing.T) {
+	actual := Heron(3.00, 4.00, 5.00)
+	assert.Equal(t, 6.00, actual)
+}
+
+func Test_GetMiddle_should_return_the_correct_string(t *testing.T) {
+	actual := GetMiddle("Apfel")
+	assert.Equal(t, "f", actual)
 }
