5cdb766a15a2595d7679d8282438287999387c27
Sven Vergin <sven.vergin@nortal.com>
2023-07-12 15:38:32 +0200

    neue Kata, erster Wurf

--- katas/katas.go
+++ katas/katas.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-//Kata: Beginner - Lost Without a Map
+// Kata: Beginner - Lost Without a Map
 func Maps(x []int) []int {
 	output := make([]int, len(x))
 
@@ -18,7 +18,7 @@ func Maps(x []int) []int {
 	return output
 }
 
-//Kata: Square(n) Sum
+// Kata: Square(n) Sum
 func SquareSum(numbers []int) int {
 	var result int = 0
 
@@ -28,12 +28,12 @@ func SquareSum(numbers []int) int {
 	return result
 }
 
-//Kata: Remove String Spaces
+// Kata: Remove String Spaces
 func NoSpace(word string) string {
 	return strings.ReplaceAll(word, " ", "")
 }
 
-//Kata: Sum of positive
+// Kata: Sum of positive
 func PositiveSum(numbers []int) int {
 	var result int = 0
 	for _, v := range numbers {
@@ -44,14 +44,14 @@ func PositiveSum(numbers []int) int {
 	return result
 }
 
-//Kata: Heron's formula
+// Kata: Heron's formula
 func Heron(a, b, c float64) (area float64) {
 	s := (a + b + c) / 2
 	area = math.Sqrt(s * (s - a) * (s - b) * (s - c))
 	return area
 }
 
-//Kata: Get the Middle Character
+// Kata: Get the Middle Character
 func GetMiddle(s string) string {
 
 	laenge := len(s)
@@ -67,7 +67,7 @@ func GetMiddle(s string) string {
 	}
 }
 
-//Kata: Vowel Count
+// Kata: Vowel Count
 func GetCount(str string) (count int) {
 	stringslice := strings.Split(str, "")
 
@@ -82,7 +82,7 @@ func GetCount(str string) (count int) {
 	return count
 }
 
-//Kata: Highest and Lowest
+// Kata: Highest and Lowest
 func HighAndLow(in string) string {
 	hlSlice := strings.Split(in, " ")
 	var min int64 = 1
@@ -99,7 +99,7 @@ func HighAndLow(in string) string {
 	return fmt.Sprint(max, " ", min)
 }
 
-//Kata: Mumbling
+// Kata: Mumbling
 func Accum(s string) string {
 	builder := strings.Builder{}
 	toAccum := strings.Split(s, "")
@@ -116,7 +116,7 @@ func Accum(s string) string {
 	return builder.String()
 }
 
-//Kata: Shortest Word
+// Kata: Shortest Word
 func FindShortest(str string) int {
 	var minLength = len(str)
 	words := strings.Split(str, " ")
@@ -130,7 +130,7 @@ func FindShortest(str string) int {
 	return minLength
 }
 
-//Kata: Two to One
+// Kata: Two to One
 func TwoToOne(s1, s2 string) string {
 	s1s := strings.Split(s1, "")
 	allLetters := append(s1s, strings.Split(s2, "")...)
@@ -150,4 +150,166 @@ func TwoToOne(s1, s2 string) string {
 	}
 
 	return builder.String()
+}
+
+// Kata: Factorial decomposition
+// https://www.codewars.com/kata/5a045fee46d843effa000070
+func Decomp(n int) string {
+
+	num := calcFalcuty(n)
+
+	primes := getPrimes(num)
+	res := make(map[int]int, len(primes))
+	count := 0
+	tmpnum := num
+
+	for i := 0; i < len(primes); i++ {
+		for {
+			if tmpnum%primes[i] != 0 {
+				break
+			}
+			tmpnum = tmpnum / primes[i]
+			count++
+		}
+		if count > 0 {
+			res[primes[i]] = count
+			count = 0
+		}
+	}
+	sb := strings.Builder{}
+	// fmt.Sprintln(pnums)
+	cnt := 0
+	for k, v := range res {
+		sb.WriteString(fmt.Sprintf("%v^%v", k, v))
+		cnt++
+		if cnt < len(res) {
+			sb.WriteString(" * ")
+		}
+	}
+	return sb.String()
+}
+
+func DecompGoRout(facultyNum int) string {
+
+	facultyResult := calcFalcuty(facultyNum)
+
+	primes := getPrimes(facultyResult)
+
+	result := make(map[int]int, len(primes))
+	countPrimeChan := make(chan primecount, 1)
+
+	for i := 0; i < len(primes); i++ {
+		go countPrimeFactors(countPrimeChan, facultyResult, primes[i])
+		p := <-countPrimeChan
+		if p.prime > 0 {
+			result[p.prime] = p.count
+		}
+
+	}
+	sb := strings.Builder{}
+	cnt := 0
+	for k, v := range result {
+		sb.WriteString(fmt.Sprintf("%v^%v", k, v))
+		cnt++
+		if cnt < len(result) {
+			sb.WriteString(" * ")
+		}
+	}
+	return sb.String()
+}
+
+type primecount struct {
+	prime int
+	count int
+}
+
+func countPrimeFactors(primeChan chan primecount, rootNum int, prime int) {
+	tmpnum := rootNum
+	result := primecount{}
+	for {
+		if tmpnum%prime != 0 {
+			break
+		}
+		tmpnum = tmpnum / prime
+		result.count++
+	}
+	if result.count > 0 {
+		result.prime = prime
+	}
+	primeChan <- result
+}
+
+func calcFalcuty(n int) int {
+	if n == 0 {
+		return 1
+	}
+	num := 1
+	for i := n; i >= 1; i-- {
+		num = num * i
+	}
+	return num
+}
+
+//primeCache := map[int]int {2:2}
+
+func getPrimes(num int) []int {
+	if num < 2 {
+		return []int{}
+	}
+	if num == 2 {
+		return []int{2}
+	}
+	knownPrimes := []int{2, 3, 5, 7, 11}
+	numToFilter := make([]int, num-4)
+
+	for i := range numToFilter {
+		numToFilter[i] = 3 + i
+	}
+
+	var relevantPrimes []int
+
+	for _, p := range knownPrimes {
+		if p <= num {
+			relevantPrimes = append(relevantPrimes, p)
+		}
+	}
+
+	result := filterPrimes(numToFilter, relevantPrimes)
+
+	return result
+}
+
+// Sieb des Erastothenes
+func filterPrimes(numToFilter, knownPrimes []int) []int {
+
+	resMap := make(map[int]int, len(knownPrimes))
+	for _, p := range knownPrimes {
+		resMap[p] = p
+	}
+	for _, n := range numToFilter {
+		if _, ok := resMap[n]; ok {
+			continue
+		}
+
+		isPrime := true
+		for _, p := range knownPrimes {
+
+			if p > n {
+				isPrime = false
+				break
+			}
+			if n%p == 0 && n != p {
+				isPrime = false
+				break
+			}
+		}
+		if isPrime {
+			resMap[n] = n
+		}
+	}
+	result := make([]int, 0, len(resMap))
+	for k := range resMap {
+		result = append(result, k)
+	}
+	return result
 }
